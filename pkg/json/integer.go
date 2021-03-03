967d2900fa04d54d2528f2168518b2fb8d72e08c
Andrey Palamarchuk <a.palamarchuk@corp.101xp.com>
2021-03-03 18:38:48 +0300

    Added JEnvInt implementation

--- pkg/json/integer.go
+++ pkg/json/integer.go
@@ -1,1 +1,33 @@
 package json
+
+import (
+	"os"
+	"strconv"
+)
+
+// JEnvInt extends base int (platform dependent) type with specific JSON unmarshalling
+type JEnvInt int64
+
+// UnmarshalJSON implements unmarshalling with env implementation
+func (val *JEnvInt) UnmarshalJSON(data []byte) error {
+	env, err := getenv(data)
+
+	if err != nil {
+		res, err := strconv.Atoi(string(env))
+
+		if err != nil {
+			return err
+		}
+
+		*val = JEnvInt(res)
+
+		return nil
+
+	}
+
+	res, err := strconv.Atoi(os.Getenv(string(env)))
+
+	*val = JEnvInt(res)
+
+	return nil
+}
